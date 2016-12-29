1f512efaa58ba01cd12da087e7f3fd0c344c0b3a
Johan Jern <johanjern95@gmail.com>
2016-12-30 00:00:03 +0200

    Added subscribed and notSubscribed channels skeletons

--- public/javascripts/chat.es6.jsx
+++ public/javascripts/chat.es6.jsx
@@ -6,15 +6,27 @@ class ChatApp extends React.Component {
         };
         const name = "username";
         const channel = "channelname";
+        const notSubscribed = ["not1", "not2"]
+        const subscribed = [{name: "sub1", lastMessage: "lmsg1"}, {name: "sub2", lastMessage: "lmsg2"}]
 
-        return <div><MessageList data={data.messages}/></div>
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <div><ChannelList notSubscribed={notSubscribed} subscribed={subscribed}/></div>
+                </div>
+                <div className="col-md-8">
+                    <div><MessageList data={data.messages}/></div>
+                    <div><MessageBox /></div>
+                </div>
+            </div>
+        )
     }
 }
 
 class MessageList extends React.Component {
     render() {
-        const msgNodes = this.props.data.map((msg) => {
-            return <ChatMessage user={msg.user} message={msg.message} />
+        const msgNodes = this.props.data.map((msg, i) => {
+            return <ChatMessage user={msg.user} message={msg.message} key={i.toString()}/>
         })
         return <div id="chat">{msgNodes}</div>
     }
@@ -31,8 +43,48 @@ class ChatMessage extends React.Component {
     }
 }
 
+class MessageBox extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <input type="text" id="messageField" ref="text" placeholder="Say something" className="form-control" />
+                <input type="button" className="btn btn-primary" value="Submit" onClick="" />
+            </div>
+        )
+    }
+}
+
+class ChannelList extends React.Component {
+    render() {
+        const notSubscribed = this.props.notSubscribed.map((ns) => {
+            return <NotSubscribed name={ns} key={ns} />
+        })
+        const subscribed = this.props.subscribed.map((s) => {
+            return <Subscribed name={s.name} lastMessage={s.lastMessage}/>
+        })
+        return <div id="channels">{notSubscribed}{subscribed}</div>
+    }
+}
+
+class NotSubscribed extends React.Component {
+    render() {
+        return <div><strong>{this.props.name}</strong></div>
+    }
+}
+
+class Subscribed extends React.Component {
+    render() {
+        return (
+            <div>
+                <strong>{this.props.name}</strong><br/>
+                {this.props.lastMessage}
+            </div>
+        )
+    }
+}
+
 const element = (
-    <div>
+    <div className="container">
         <h1>Hello, world!</h1>
         <ChatApp />
     </div>
