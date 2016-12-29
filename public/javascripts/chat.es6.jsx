c18539cbed1bea86bdaeff5088897d01ac5cb7b2
Johan Jern <johanjern95@gmail.com>
2016-12-29 20:50:34 +0200

    Created static ChatApp, MessageList and ChatMessage components

--- public/javascripts/chat.es6.jsx
+++ public/javascripts/chat.es6.jsx
@@ -1,14 +1,44 @@
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello, world!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    );
-    ReactDOM.render(
-        element,
-        document.getElementById('chat-app')
-    );
+
+class ChatApp extends React.Component {
+    render() {
+        const data = {
+            messages: [{user: "user1", message: "message1"}, {user: "user2", message: "message2"}]
+        };
+        const name = "username";
+        const channel = "channelname";
+
+        return <div><MessageList data={data.messages}/></div>
+    }
 }
 
-setInterval(tick, 1000);
+class MessageList extends React.Component {
+    render() {
+        const msgNodes = this.props.data.map((msg) => {
+            return <ChatMessage user={msg.user} message={msg.message} />
+        })
+        return <div id="chat">{msgNodes}</div>
+    }
+}
+
+class ChatMessage extends React.Component {
+    render() {
+        return (
+            <div className="">
+                <strong>{this.props.user}: </strong>
+                {this.props.message}
+            </div>
+        )
+    }
+}
+
+const element = (
+    <div>
+        <h1>Hello, world!</h1>
+        <ChatApp />
+    </div>
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById('chat-app')
+);
