cad0147004f68bc1af1f5d3de8a6940ccc9a175a
Johan Jern <johanjern95@gmail.com>
2017-01-14 13:18:10 +0200

    Configure webpack for production

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,6 +21,19 @@ const config = {
             }
         ]
     },
+    plugins:[
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress:{
+                warnings: true
+            },
+            comments: false
+        })
+    ],
 }
 
 module.exports = config
